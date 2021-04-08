5cfb4da5d7affda8cf3c27fb7e59ccdbefe45ae3
msk- <mattkingston@gmail.com>
2021-04-08 02:32:20 +0100

    Added logging, exported main application start function for testing.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@ const Client = require('./lib/Client');
 const config = require('./config/client');
 const roles = require('./imports/roles');
 const users = require('./imports/users');
+const { createApplication } = require('./lib/service_provider');
 
-const instance = new Client(config);
+async function populate(conf) {
+    console.log('Beginning WSO2 IS populate. Config:', conf);
 
-async function importData() {
+    createApplication({
+        ...conf.application,
+        ...conf.http,
+        ...conf.authentication.credentials,
+    });
+
+    const instance = new Client(conf);
+    console.log('Adding users');
     const addedUsers = await instance.addUsers(users);
+    console.log('Attaching user ids to roles');
     const rolesWithUserIds = Client.attachUserIdsToRoles(addedUsers, roles);
+    console.log('Adding roles');
     await instance.addRoles(rolesWithUserIds);
 }
 
-importData();
+module.exports = { populate };
+
+if (require.main === module) {
+    populate(config);
+}
