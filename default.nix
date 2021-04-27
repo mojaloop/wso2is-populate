8d87c4860d7d48b4c2e5308dfa4645d8876e9ade
msk- <mattkingston@gmail.com>
2021-04-27 17:44:17 +0100

    CI WIP

--- default.nix
+++ default.nix
@@ -1,16 +1,3 @@
-{ nixpkgs ? import <nixpkgs> {  } }: {
-  pkgs = [
+{ nixpkgs ? import <nixpkgs> {  } }: [
     nixpkgs.terraform_0_14
-  ];
-}
-
-# let
-#   pkgs = [
-#     nixpkgs.nodejs
-#   ];
-#
-# in
-#   nixpkgs.stdenv.mkDerivation {
-#     name = "env";
-#     buildInputs = pkgs;
-#   }
+]
