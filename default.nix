d0ece7b28353668245bdaa47125340eca9428c5d
msk- <mattkingston@gmail.com>
2021-05-10 13:59:38 +0100

    Pin packages in default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {  } }: [
+{ nixpkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/8e4fe32876ca15e3d5eb3ecd3ca0b224417f5f17.tar.gz) { } }: [
     nixpkgs.terraform_0_14
     nixpkgs.python39Packages.jsonschema
     nixpkgs.jq
