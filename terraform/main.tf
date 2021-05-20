7d08d9a58025b92f0e4455e284121b4d6c08677a
msk- <mattkingston@gmail.com>
2021-05-20 23:06:32 +0100

    Tidy up docker container stuff. Give module action a marginally better name.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,4 @@
-# resource "docker_container" "wso2ispopulate" {
-#   name            = "wso2ispopulate"
-#   image           = docker_image.wso2ispopulate.latest
-#   restart         = "on-failure"
-#   max_retry_count = 10
-#   attach          = true
-#   logs            = true
-#   must_run        = true
-#   env = [
-#     "WSO2_HOST=${var.wso2_host}",
-#     "AUTHENTICATION_CREDENTIALS_USERNAME=${var.admin_user}",
-#     "AUTHENTICATION_CREDENTIALS_PASSWORD=${var.admin_password}",
-#     "APPLICATION_NAME=${var.wso2_oauth2_application_name}",
-#     "AUTH_SERVER_CLIENTKEY=${var.auth_server_clientkey}",
-#     "AUTH_SERVER_CLIENTSECRET=${var.auth_server_clientsecret}",
-#   ]
-#   upload {
-#     content = jsonencode(var.portal_users)
-#     file    = "/src/imports/users.json"
-#   }
-# }
-#
-# resource "docker_image" "wso2ispopulate" {
-#   name = "ghcr.io/modusintegration/wso2is-populate:2.0.2"
-# }
-
-resource "null_resource" "create_artifacts_return_credentials" {
+resource "null_resource" "mutate_wso2is_configuration" {
   provisioner "local-exec" {
 
     command = <<EOT
